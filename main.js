29f2270ded1ddf647a7b543b705b99102a1831b2
NIKIDIGI <nikidigi.yt@gmail.com>
2023-07-03 05:59:33 +0000

    add upgrade type

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-const COINS_PER_LEVEL = 150;
-const TOKENS_PER_LEVEL = 2;
-
 function calc() {
+    const coinsPerLevel = 150;
+    const tokensPerLevel = $('#tokens-per-level').val() * 1;
     const from = $('#level-from').val() * 1;
     const to = $('#level-to').val() * 1;
     let coins = 0;
     let tokens = 0;
 
     for (let i = from; i < to; i += 1) {
-        coins += COINS_PER_LEVEL * i;
-        tokens += TOKENS_PER_LEVEL * i;
+        coins += coinsPerLevel * i;
+        tokens += tokensPerLevel * i;
     }
 
     $('#coins').val(coins);
